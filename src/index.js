ca1972e4fadbbd74d57e6c8b2a978f7728a31ff5
VegadelaLyra <lyradeperseo@gmail.com>
2022-12-10 04:26:10 -0500

    Deployed to netlify! :D

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Template from '@templates/Template.js'
 import '@styles/main.css'
 import '@styles/vars.styl'
-
+// down here, syntax to autorun function without calling is used
+// (async fn(){})()
 (async function App() {
   const main = null || document.getElementById('main')
   main.innerHTML = await Template()
--- src/templates/Template.js
+++ src/templates/Template.js
@@ -26,13 +26,13 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
+          <a href="https://www.linkedin.com/in/vegadelalyra/">
             <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
+          <a href="https://github.com/vegadelalyra">
             <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
+          <a href="https://www.instagram.com/loversjustforfun/">
             <img src="${instagram}" />
           </a>
         </div>
